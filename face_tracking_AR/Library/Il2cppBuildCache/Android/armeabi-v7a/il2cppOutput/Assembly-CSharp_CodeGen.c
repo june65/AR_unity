8d10ba34cc6c7c76fc07a63cd0401163062f9677
june65 <june0100726@gmail.com>
2022-07-15 20:48:36 +0900

    Issue(#3) Add face_tracking_project

---
+++ face_tracking_AR/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ARFaceSwitch::Start()
+extern void ARFaceSwitch_Start_m14BA444A000731EE947B4B2A7625716CBD22E3FD (void);
+// 0x00000002 System.Void ARFaceSwitch::SwithchFaces()
+extern void ARFaceSwitch_SwithchFaces_m526EDEA7BC620C7D4444A29C3E517B6133F4E261 (void);
+// 0x00000003 System.Void ARFaceSwitch::Update()
+extern void ARFaceSwitch_Update_m55F862EB04EC34151C8F46F972336207D8F13906 (void);
+// 0x00000004 System.Void ARFaceSwitch::.ctor()
+extern void ARFaceSwitch__ctor_mE54F4D2F651EE8F66052C556F890F583036D9FEF (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	ARFaceSwitch_Start_m14BA444A000731EE947B4B2A7625716CBD22E3FD,
+	ARFaceSwitch_SwithchFaces_m526EDEA7BC620C7D4444A29C3E517B6133F4E261,
+	ARFaceSwitch_Update_m55F862EB04EC34151C8F46F972336207D8F13906,
+	ARFaceSwitch__ctor_mE54F4D2F651EE8F66052C556F890F583036D9FEF,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	1850,
+	1850,
+	1850,
+	1850,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
